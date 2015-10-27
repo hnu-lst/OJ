7f2a67704229a23e9e7ab3dc7e227b18afba289c
hnu-lst <lst@hnu.edu.cn>
2015-10-27 15:43:38 +0800

    find median from data stream

---
+++ leetcode/295/MedianFinder.java
@@ -0,0 +1,42 @@
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+class MedianFinder {
+
+    private Comparator<Integer> comparator = new Comparator<Integer>() {
+        @Override
+        public int compare(Integer i1, Integer i2) {
+            return i2.compareTo(i1);
+        }
+    };
+
+    private PriorityQueue<Integer> minHeap = new PriorityQueue<Integer>();
+    private PriorityQueue<Integer> maxHeap = new PriorityQueue<Integer>(comparator);
+
+    public void addNum(int num) {
+        if (maxHeap.size() == minHeap.size()) {
+            if (minHeap.size() != 0 && num > minHeap.peek()) {
+                maxHeap.offer(minHeap.poll());
+                minHeap.offer(num);
+            } else {
+                maxHeap.offer(num);
+            }
+        } else {
+            if (num < maxHeap.peek()) {
+                minHeap.offer(maxHeap.poll());
+                maxHeap.offer(num);
+            } else {
+                minHeap.offer(num);
+            }
+        }
+
+    }
+
+    public double findMedian() {
+        if (maxHeap.size() == minHeap.size()) {
+            return (double) (minHeap.peek() + maxHeap.peek()) / 2;
+        } else {
+            return maxHeap.peek();
+        }
+    }
+};
