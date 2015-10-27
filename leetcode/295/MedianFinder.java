30274d4feba295499fb6aa28ff1558cedc403f62
hnu-lst <lst@hnu.edu.cn>
2015-10-27 15:46:18 +0800

    find median from data stream

--- leetcode/295/MedianFinder.java
+++ leetcode/295/MedianFinder.java
@@ -36,7 +36,7 @@ class MedianFinder {
         if (maxHeap.size() == minHeap.size()) {
             return (double) (minHeap.peek() + maxHeap.peek()) / 2;
         } else {
-            return (double)maxHeap.peek();
+            return (double) maxHeap.peek();
         }
     }
 };
