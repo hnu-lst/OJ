694b9c8bc2033e3a2ab4c8a8ffea05476eed4b28
hnu-lst <lst@hnu.edu.cn>
2015-10-29 16:04:21 +0800

    remove duplicates from sorted list II

--- leetcode/82/Solution.java
+++ leetcode/82/Solution.java
@@ -1,11 +1,3 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) { val = x; }
- * }
- */
 public class Solution {
     public ListNode deleteDuplicates(ListNode head) {
         ListNode dummy = new ListNode(0), fast = head, slow = dummy;
